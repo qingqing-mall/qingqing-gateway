b181348945e5f2cb43ec021a6d81429c4b3a6855
liaoyanbo <liaoyanbo@cloudwalk.cn>
2019-05-03 11:19:16 +0800

    Initial application generated by JHipster-5.7.2

---
+++ src/main/java/com/liaoyb/qingqing/gateway/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.liaoyb.qingqing.gateway.config.audit;
---
+++ src/main/java/com/liaoyb/qingqing/gateway/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.liaoyb.qingqing.gateway.web.rest.vm;
